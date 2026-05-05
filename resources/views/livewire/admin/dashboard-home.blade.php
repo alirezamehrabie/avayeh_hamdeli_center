29c7ecf69a6935b0ac8e3b4300c9329019d7ea5c
Alireza Mehrabi <62432124+alirezamehrabie@users.noreply.github.com>
2026-05-05 22:43:42 +0330

    Update dashboard-home.blade.php

--- resources/views/livewire/admin/dashboard-home.blade.php
+++ resources/views/livewire/admin/dashboard-home.blade.php
@@ -171,13 +171,13 @@
                                     icon="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
 
                                 <livewire:admin.dashboard.stat-card
-                                    title="خانوار"
+                                    title="خانوار فعال"
                                     :value="$guardianCount  . ' نفر '"
                                     color="blue"
                                     icon="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
 
                                 <livewire:admin.dashboard.stat-card
-                                    title="مددکاران"
+                                    title="مددکاران فعال"
                                     :value="$totalSocialWorkers  . ' نفر '"
                                     color="puplre"
                                     icon="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
