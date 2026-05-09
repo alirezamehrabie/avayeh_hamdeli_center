2fa34d4272c2e8931bc1aa2f69cbb3d0feb04bf3
Alireza Mehrabi <62432124+alirezamehrabie@users.noreply.github.com>
2026-05-09 14:38:47 +0330

    no-images png send

--- resources/views/livewire/people/create-person.blade.php
+++ resources/views/livewire/people/create-person.blade.php
@@ -478,7 +478,7 @@
                                                 @elseif($mode == 'edit' && $person && $person->photo_id_card)
                                                     <img src="{{ asset($person->photo_id_card) }}" id="captured-img-id-card" class="img-thumbnail" style="max-height: 150px;">
                                                 @else
-                                                    <img src="{{ asset('assets/images/no-image.png') }}" id="captured-img-id-card" class="img-thumbnail" style="max-height: 150px;">
+                                                    <img src="{{ asset('images/no-images.png') }}" id="captured-img-id-card" class="img-thumbnail" style="max-height: 150px;">
                                                 @endif
                                             </div>
                                             <div class="mt-2 text-center">
@@ -534,7 +534,7 @@
                                                 @elseif($mode == 'edit' && $person && $person->photo_birth_certificate)
                                                     <img src="{{ asset($person->photo_birth_certificate) }}" id="captured-img-birth-cert" class="img-thumbnail" style="max-height: 150px;">
                                                 @else
-                                                    <img src="{{ asset('images/no-image.png') }}" id="captured-img-birth-cert" class="img-thumbnail" style="max-height: 150px;">
+                                                    <img src="{{ asset('images/no-images.png') }}" id="captured-img-birth-cert" class="img-thumbnail" style="max-height: 150px;">
                                                 @endif
                                             </div>
                                             <div class="mt-2 text-center">
@@ -590,7 +590,7 @@
                                                 @elseif($mode == 'edit' && $person && $person->profile_photo)
                                                     <img src="{{ asset($person->profile_photo) }}" id="captured-img-profile" class="img-thumbnail" style="max-height: 200px;">
                                                 @else
-                                                    <img src="{{ asset('images/no-image.png') }}" id="captured-img-profile" class="img-thumbnail" style="max-height: 200px;">
+                                                    <img src="{{ asset('images/no-images.png') }}" id="captured-img-profile" class="img-thumbnail" style="max-height: 200px;">
                                                 @endif
                                             </div>
                                             <div class="mt-2 text-center">
@@ -1583,7 +1583,7 @@
                                                     <img src="{{ asset($person->supportCoverage->support_card_image) }}" id="captured-img-support-card" class="img-thumbnail" style="max-height: 150px;">
                                                 @else
                                                     {{-- تصویر پیش‌فرض --}}
-                                                    <img src="{{ asset('images/no-image.png') }}" id="captured-img-support-card" class="img-thumbnail" style="max-height: 150px;">
+                                                    <img src="{{ asset('images/no-images.png') }}" id="captured-img-support-card" class="img-thumbnail" style="max-height: 150px;">
                                                 @endif
                                             </div>
                                             <div class="mt-2 text-center">
@@ -1839,7 +1839,7 @@
         const preview = document.getElementById('photo-preview-' + cameraId);
         if (preview) {
             preview.innerHTML = `
-                <img src="{{ asset('images/no-image.png') }}"
+                <img src="{{ asset('images/no-images.png') }}"
                      id="captured-img-${cameraId}"
                      class="img-thumbnail"
                      style="max-height: 150px;">
