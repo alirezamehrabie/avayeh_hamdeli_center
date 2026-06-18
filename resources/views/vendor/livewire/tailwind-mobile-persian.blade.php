98da047529ab97af01c7e5485c9fcf23b598bbb9
Alireza Mehrabi <62432124+alirezamehrabie@users.noreply.github.com>
2026-06-19 02:52:05 +0330

    Fix: Correct Persian Paginator Localization Encoding

--- resources/views/vendor/livewire/tailwind-mobile-persian.blade.php
+++ resources/views/vendor/livewire/tailwind-mobile-persian.blade.php
@@ -17,11 +17,11 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                 <span>
                     @if ($paginator->onFirstPage())
                         <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300">
-                            قبلی
+                            {!! __('pagination.previous') !!}
                         </span>
                     @else
                         <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-blue-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300">
-                            قبلی
+                            {!! __('pagination.previous') !!}
                         </button>
                     @endif
                 </span>
@@ -29,11 +29,11 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                 <span>
                     @if ($paginator->hasMorePages())
                         <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-blue-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300">
-                            بعدی
+                            {!! __('pagination.next') !!}
                         </button>
                     @else
                         <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md dark:text-gray-600 dark:bg-gray-800 dark:border-gray-600">
-                            بعدی
+                            {!! __('pagination.next') !!}
                         </span>
                     @endif
                 </span>
@@ -43,17 +43,17 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
                 <div>
                     <p class="text-sm text-gray-700 leading-5 dark:text-gray-400">
                         @if ($paginator->firstItem())
-                            <span>Ù†Ù…Ø§ÛŒØ´</span>
+                            <span>نمایش</span>
                             <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                            <span>ØªØ§</span>
+                            <span>تا</span>
                             <span class="font-medium">{{ $paginator->lastItem() }}</span>
-                            <span>Ø§Ø²</span>
+                            <span>از</span>
                             <span class="font-medium">{{ $paginator->total() }}</span>
-                            <span>Ù…ÙˆØ±Ø¯</span>
+                            <span>مورد</span>
                         @else
-                            <span>Ù†Ù…Ø§ÛŒØ´</span>
+                            <span>نمایش</span>
                             <span class="font-medium">{{ $paginator->count() }}</span>
-                            <span>Ù…ÙˆØ±Ø¯</span>
+                            <span>مورد</span>
                         @endif
                     </p>
                 </div>
